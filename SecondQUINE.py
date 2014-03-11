d7d7cd06b323c71cfdf39af0d9b2379e33a297f8
Zhichun Li <liyaya95@gmail.com>
2014-03-11 14:31:26 -0500

    Update SecondQUINE.py

--- SecondQUINE.py
+++ SecondQUINE.py
@@ -4,3 +4,5 @@ def main():
     print x[4:15] + chr(10) + chr(32) + chr(32) + chr(32) + x[:4] + chr(34) + x + chr(34) + chr(10) + chr(32) + x[15:]
 
 main()
+#could be improved and will be improved. 
+#pondering if I should write one in sml or java. 
