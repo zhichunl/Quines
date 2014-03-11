6ed191e8f82b49d9b6338dc8f967b4a079263c9a
zhichunl <liyaya95@gmail.com>
2014-03-11 14:25:48 -0500

    My Own Quine

---
+++ SecondQUINE.py
@@ -0,0 +1,6 @@
+#my own quine in my own style without really knowing python
+def main():
+    x = "x = def main():  print x[4:15] + chr(10) + chr(32) + chr(32) + chr(32) + x[:4] + chr(34) + x + chr(34) + chr(10) + chr(32) + x[15:]"
+    print x[4:15] + chr(10) + chr(32) + chr(32) + chr(32) + x[:4] + chr(34) + x + chr(34) + chr(10) + chr(32) + x[15:]
+
+main()
