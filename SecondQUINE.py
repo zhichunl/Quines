faf763203b6dbd7fa7641cb357505859c99374e6
Zhichun Li <liyaya95@gmail.com>
2017-11-22 22:15:31 -0600

    Update SecondQUINE.py

--- SecondQUINE.py
+++ SecondQUINE.py
@@ -1,8 +1,7 @@
-#my own quine in my own style without really knowing python
+# my quine: not as elegant
 def main():
     x = "x = def main():  print x[4:15] + chr(10) + chr(32) + chr(32) + chr(32) + x[:4] + chr(34) + x + chr(34) + chr(10) + chr(32) + x[15:]"
     print x[4:15] + chr(10) + chr(32) + chr(32) + chr(32) + x[:4] + chr(34) + x + chr(34) + chr(10) + chr(32) + x[15:]
 
 main()
-#could be improved and will be improved. 
-#pondering if I should write one in sml or java. 
+
