14b96066f7ec980848f945ef5d90e983ed0ce50c
Zhichun Li <liyaya95@gmail.com>
2017-11-22 22:17:33 -0600

    Update SecondQUINE.py

--- SecondQUINE.py
+++ SecondQUINE.py
@@ -1,4 +1,4 @@
-# my quine: not as elegant
+# my quine: written at a time without familiarity in python.
 def main():
     x = "x = def main():  print x[4:15] + chr(10) + chr(32) + chr(32) + chr(32) + x[:4] + chr(34) + x + chr(34) + chr(10) + chr(32) + x[15:]"
     print x[4:15] + chr(10) + chr(32) + chr(32) + chr(32) + x[:4] + chr(34) + x + chr(34) + chr(10) + chr(32) + x[15:]
